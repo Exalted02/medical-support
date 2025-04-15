f1600262fbe143b860398ee23b1adee91bbd6c2f
Exalted02 <exaltedsol02@gmail.com>
2025-04-15 18:41:44 +0530

    up

--- resources/views/client_chat_reason.blade.php
+++ resources/views/client_chat_reason.blade.php
@@ -47,7 +47,7 @@
 	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Create Challenge</h5>
+				<h5 class="modal-title">Create Reason</h5>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
